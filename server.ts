5fbd456d28ec2dfa1cb0e87defed8fa7fca1237c
stacc <stacc@staccs-MacBook-Air.local>
2022-06-15 14:29:19 -0300

    2c

--- server.ts
+++ server.ts
@@ -573,4 +573,4 @@ astring+=key + ": " + totals[key] + " && "
 //console.log(astring)
   res.status(200).send(astring);
   })
-app.listen(3000, () => console.log(`Listening on port ${3000}`));
+app.listen(process.env.PORT || 3000, () => console.log(`Listening on port ${3000}`));
